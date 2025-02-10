174bc1df5077fb9395277f04061bf0f96b7494c9
Yeamin Hossain <yeamin044@gmail.com>
2025-02-10 11:21:34 +0600

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MediBot
+### Steps
+Clone the repo
+
+### Step 1: Conda env creation
+
+'''bash
+conda create -n medibot python=3.10 -y
+'''
+
+conda activate medibot
+
+### Step 2: Install the requirements
+
+pip install -r requrirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "MediBot",
+    version= '0.0.1',
+    author= 'Yeamin',
+    author_email='mdyeaminhossain25@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
